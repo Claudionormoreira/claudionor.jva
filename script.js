51aa7bf975d3a65f24488c2ea07b8c1a932195c4
Claudionor=MOREIRA <claudionormoreirarj@gmail.com>
2025-12-10 07:12:51 +0000

    novo comit

--- script.js
+++ script.js
@@ -2,18 +2,32 @@ function adicionarTarefa() {
 
     //recebe valor do input do usuário
     let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
+    let tarefa = inputTarefa.value.trim()
 
-    //cria novo item (li) e insere na (lista ul)
+    // Se o valor do input for vazio, então mostra uma mensagem de erro para o usuário.
+    if (tarefa == ""){
+        //mostre uma mensagem de erro
+        let mensagemErro = "Digite uma tarefa para adicioná-las a sua lista"
+        document.getElementById("mensagem").textContent = mensagemErro
+        
+    } else { 
+        //mensagem de tarefa adicionada com sucesso
+    let mensagemSucesso = "Tarefa adicionada com sucesso!"
+    document.getElementById("mensagem").textContent = mensagemSucesso
+
+     //cria novo item (li) e insere na (lista ul)
     let listaTarefas = document.getElementById("listaTarefas")
     let novaTarefa = document.createElement("li")
     novaTarefa.textContent = tarefa
     listaTarefas.appendChild(novaTarefa)
 
-    //mensagem de tarefa adicionada com sucesso
-    let mensagem = "Tarefa adicionada com sucesso!"
-    document.getElementById("mensagem").textContent = mensagem
+    }   
 
+   
     //limpa o input do usuário
     inputTarefa.value = ""
+
+    
+
+
 }
